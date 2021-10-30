0c60eb75a86016744ed65e8f640bd77664653091
ArthurWuTW <lion811004@gmail.com>
2021-10-31 07:14:21 +0800

    agent sends certificate req

--- agent/installation/script/install-puppet-agent-ubuntu-16-04.sh
+++ agent/installation/script/install-puppet-agent-ubuntu-16-04.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 TIME_ZONE=Asia/Taipei
-HOST_NAME=Puppet-Agent
+HOST_NAME=Puppet-Agent-Machine1
 OLD_HOST_NAME=$(hostname)
+PUPPET_MASTER_IP=10.1.1.2
 
 # Show info
 echo "-------------------------------------------------------------"
@@ -13,14 +14,17 @@ echo "-------------------------------------------------------------"
 # Set Timezone
 sudo timedatectl set-timezone $TIME_ZONE
 
-# Correct New Host Name
+# Add New Host Name
 sudo /bin/bash -c 'echo "127.0.1.1   '$HOST_NAME'" >> /etc/hosts'
 
-# Set Hostname
+# Set New Hostname
 sudo hostnamectl set-hostname $HOST_NAME
 sleep 5
 echo "[Warning]Bug: Cannot resolved new hostname first time"
 sudo hostnamectl set-hostname $HOST_NAME
+
+# Set Puppet Master Hostname
+sudo /bin/bash -c 'echo "'$PUPPET_MASTER_IP'   puppet puppet-server" >> /etc/hosts'
 
 # Install Chrony
 sudo apt-get update
@@ -37,4 +41,12 @@ sudo dpkg -i puppetlabs-release-pc1-xenial.deb
 sudo apt-get update
 sudo apt-get install -y puppet-agent --allow-unauthenticated
 
+sudo service puppet stop
 sudo service puppet start
+
+echo "=============== Puppet Agent Installed Successfully! ============="
+
+echo "Please wait a minute, and go to master and type"
+echo ">>sudo /opt/puppetlabs/puppet/bin/puppet cert list"
+echo "It should have cert request!"
+
