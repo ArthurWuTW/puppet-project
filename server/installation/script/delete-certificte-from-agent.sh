1691ebe5941995dd10cd0d1939dba0961402a6aa
ArthurWuTW <lion811004@gmail.com>
2021-10-31 08:22:59 +0800

    server add some script

---
+++ server/installation/script/delete-certificte-from-agent.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo /opt/puppetlabs/puppet/bin/puppet cert clean <puppet-agent-machine1>
--- server/installation/script/sign-certificate-request-from-agent.sh
+++ server/installation/script/sign-certificate-request-from-agent.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Show both signed and unsigned certificate
+sudo /opt/puppetlabs/puppet/bin/puppet cert list --all
+
+# Show unsigned certificate
 sudo /opt/puppetlabs/puppet/bin/puppet cert list
+
 sudo /opt/puppetlabs/puppet/bin/puppet cert sign <agent-host-name>
